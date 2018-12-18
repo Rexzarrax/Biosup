aac3cfdfa8953217ad853591c8efc7477191d5e8
Jordan <Jordancattwood@hotmail.com>
2018-12-18 23:21:25 +1100

    0.11
    Version with only asrock models

--- pcpptest.py
+++ pcpptest.py
@@ -4,7 +4,7 @@ from PCPartPicker_API import pcpartpicker as pcpp
 mobo_count = pcpp.productLists.totalPages("motherboard")
 print("Total Mobo pages:", mobo_count)
 
-# Pull info from page 1 of CPUs
+#Pull info from page 1 of CPUs
 for page in range(0, 1):
     skuName = pcpp.productLists.getProductList("motherboard", page)
     # Print the names and prices of all the CPUs on the page
