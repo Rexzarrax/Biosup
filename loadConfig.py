da5599a17a37a965bea61252f963d4fccd22d126
Jordan <Jordancattwood@hotmail.com>
2019-02-19 10:33:32 +1100

    v1-7-1d
    -base gui implemented
    -fixed config loader for both methods of vendor data

--- loadConfig.py
+++ loadConfig.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 import os
+from json import load
 
 class loadConfig:
     def __init__(self, configfile):
@@ -16,38 +17,37 @@ class loadConfig:
             self.sleepTimer = int(config_object["SETTINGS"]["sleeptimer"])
             self.sleepwait = int(config_object["SETTINGS"]["sleepwait"])
             self.vendor = (config_object["SETTINGS"]["vendor"]).split(",")
-            self.vendorSort = (config_object["SETTINGS"]["vendorSort"].split(","))
+            self.allowedExtras = (config_object["SETTINGS"]["allowedChipsetsAddon"])
+            self.saveState = (config_object["SETTINGS"]["saveState"])
+
             try:
                 self.AMDallowedchipsets = (config_object["SETTINGS"]["allowedChipsetsAMD"].split(","))
-                self.AMDallowedchipsets.sort()
                 self.INTELallowedchipsets = (config_object["SETTINGS"]["allowedChipsetsIntel"].split(","))
-                self.INTELallowedchipsets.sort()
                 self.allowedChipsets = (self.AMDallowedchipsets+self.INTELallowedchipsets)
             except:
                 self.allowedChipsets = (config_object["SETTINGS"]["allowedChipsets"].split(","))
             self.allowedChipsets.sort()
-            
             try:
-                self.allowedExtras = (config_object["SETTINGS"]["allowedChipsetsAddon"])
+                self.vendorSort = (config_object["SETTINGS"]["vendorSort"].split(","))
                 self.vendorDownloadURLbase = (config_object["SETTINGS"]["vendorDownloadURLbase"].split(","))
                 self.vendorURLaddon = (config_object["SETTINGS"]["vendorURLaddon"].split(","))
-                self.saveState = (config_object["SETTINGS"]["saveState"])
                 for x in range (len(self.vendor)):
-                    self.allvendordata[self.vendor[x]] = {'vendorSort':self.vendorSort[x],
-                                    'vendorDownloadURLbase':self.vendorDownloadURLbase[x],
-                                    'vendorURLaddon':self.vendorURLaddon[x]}  
-            except:
+                    self.allvendordata[self.vendor[x]] = {"vendorSort":self.vendorSort[x],
+                                    "vendorDownloadURLbase":self.vendorDownloadURLbase[x],
+                                    "vendorURLaddon":self.vendorURLaddon[x]}  
+            except Exception as e: 
+                print("Failure in Loading vendor data from config "+str(e))
                 try:
                     self.datapath = os.path.join(os.getcwd(), os.path.dirname(__file__))+"\\vendorInfo.txt"
-                    with open (self.datapath,"w") as infile:
-                        self.allvendordata = infile.readlines()
-                except:
-                    print("Error loading config")
+                    with open (self.datapath) as infile:
+                        self.allvendordata = load(infile)
+                        infile.close()
+                except Exception as e: 
+                    print("Failure in loading vendors "+str(e))
                     exit()
-
-
             print(str(self.allvendordata))
-        except:
+        except Exception as e: 
+            print("Failure in loading vendors "+str(e))
             input("Error: Missing or Invalid configuration file(config.ini)")
             exit()
 
@@ -59,9 +59,12 @@ class loadConfig:
         print(" >Sleep Timer: "+ str(self.sleepTimer))
         print(" >Sleep Wait: "+ str(self.sleepwait))
         print(" >Vendor Array: "+str(self.vendor))
-        print(" >Vendor Web Selector: "+str(self.vendorSort))
+        try:
+            print(" >Vendor Web Selector: "+str(self.vendorSort))
+            print(" >Additions for URL:"+str(self.vendorURLaddon))
+            print(" >Download URL base: "+str(self.vendorDownloadURLbase))
+        except:
+            print(str(self.allvendordata))
         print(" >Allowed Chipsets: "+str(self.allowedChipsets))
         print(" >Allowed Extras: "+str(self.allowedExtras))
-        print(" >Download URL base: "+str(self.vendorDownloadURLbase))
-        print(" >Additions for URL:"+str(self.vendorURLaddon))
         print("Configuration Loaded...")
